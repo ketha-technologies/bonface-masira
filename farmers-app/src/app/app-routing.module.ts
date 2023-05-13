4074a5075f92da980ba8235c261687c9c9ab8a4f
Bonface Masira <bonfacemasira@gmail.com>
2023-05-13 20:55:17 +0300

    add routing and define it accordingly

--- farmers-app/src/app/app-routing.module.ts
+++ farmers-app/src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { NavbarComponent } from './navbar/navbar.component';
 import { FarmersCollectionComponent } from './farmers-collection/farmers-collection.component';
 import { ChartComponent } from './chart/chart.component';
 
 const routes: Routes = [
   // { path: '', component: FarmersCollectionComponent },
-  { path: 'farmers-collection', component: FarmersCollectionComponent },
+  { path: 'collections', component: FarmersCollectionComponent },
   { path: 'chart', component: ChartComponent },
 ];
 
--- farmers-app/src/app/app.module.ts
+++ farmers-app/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FarmersCollectionComponent } from './farmers-collection/farmers-collection.component';
 import { ChartComponent } from './chart/chart.component';
+import { IntroComponent } from './intro/intro.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FarmersCollectionComponent,
-    ChartComponent
+    ChartComponent,
+    IntroComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
