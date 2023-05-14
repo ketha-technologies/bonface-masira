f557c89b89b0a63e55e5515e31eda4b291e97934
Bonface Masira <bonfacemasira@gmail.com>
2023-05-14 20:25:04 +0300

    style the footer and header and add a background image for the main page

--- farmers-app/src/app/app-routing.module.ts
+++ farmers-app/src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { FarmersCollectionComponent } from './farmers-collection/farmers-collection.component';
 import { ChartComponent } from './chart/chart.component';
+import { IntroComponent } from './intro/intro.component';
 
 const routes: Routes = [
-  // { path: '', component: FarmersCollectionComponent },
+  { path: '', component: IntroComponent },
   { path: 'collections', component: FarmersCollectionComponent },
   { path: 'chart', component: ChartComponent },
 ];
